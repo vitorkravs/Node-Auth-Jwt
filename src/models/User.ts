2b080af1872e94cbafbf481b455003fdbc45a191
vitor kravszenko <vitorkravs3@gmail.com>
2024-01-16 18:14:28 -0300

    feat: criação do Modelo dos Usuários

---
+++ src/models/User.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+interface User {
+  name: String;
+  email: String;
+  password: String;
+}
+
+const UserDataSchema: Schema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  { versionKey: false }
+);
+
+export default mongoose.model<User>("User", UserDataSchema);
